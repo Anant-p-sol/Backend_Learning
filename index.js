18d6aa23cae263d14f855a9d8bcc45083d66a1a0
Anant Kumar <adwayaanant@gmail.com>
2026-01-31 09:57:39 +0530

    response based onroute

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to the Home Page");
+    
+  }else if (req.url === "/about") {
+    res.end("This is the About Page");
+    
+  } else if (req.url === "/contact") {
+    res.end("Contact us at contact@example.com"); 
+  } else {
+    res.end("404 Page Not Found");
+  }
   res.end("Jay Guru");
 });
 
